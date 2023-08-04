f834e465c5d9b5d55fe1cf5a66e3bffbda5c9e15
vivianlee03 <vivian@u.sogang.ac.kr>
2023-08-04 22:51:56 +0900

    debug AppLoading warning issue & change to SplashScreen

--- App.js
+++ App.js
@@ -1,38 +1,55 @@
-import AppLoading from "expo-app-loading";
+import React, { useCallback, useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
 import { StyleSheet, Text, View } from "react-native";
 import LoggedOutNav from "./navigators/LoggedOutNav";
 import { NavigationContainer } from "@react-navigation/native";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
-  const onFinish = () => setLoading(false);
-  const preload = () => {
-    const fontsToLoad = [Ionicons.font];
-    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
-    const imagesToLoad = [
-      require("./assets/logo.png"),
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png",
-    ];
-    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
-    return Promise.all([...fontPromises, ...imagePromises]);
-  };
-  if (loading) {
-    return (
-      <AppLoading
-        startAsync={preload}
-        onError={console.warn}
-        onFinish={onFinish}
-      />
-    );
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+
+        const fontsToLoad = [Ionicons.font];
+        const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+        const imagesToLoad = [
+          require("./assets/logo.png"),
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png",
+        ];
+        const imagePromises = imagesToLoad.map((image) =>
+          Asset.loadAsync(image)
+        );
+        await Promise.all([...fontPromises, ...imagePromises]);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+        SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
+
   return (
     <NavigationContainer>
-      <LoggedOutNav />
+      <LoggedOutNav onLayoutRootView={onLayoutRootView} />
     </NavigationContainer>
   );
 }
