09861981d23e93a13d1ef9033df3e7cced679aa1
vivianlee03 <vivian@u.sogang.ac.kr>
2023-08-10 12:10:25 +0900

    change top icon & modify SelectPhoto screen -#18.2

--- navigators/UploadNav.js
+++ navigators/UploadNav.js
@@ -3,6 +3,7 @@ import React from "react";
 import SelectPhoto from "../screens/SelectPhoto";
 import TakePhoto from "../screens/TakePhoto";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createMaterialTopTabNavigator();
 const Stack = createStackNavigator();
@@ -21,8 +22,24 @@ const UploadNav = () => {
     >
       <Tab.Screen name="Select">
         {() => (
-          <Stack.Navigator>
-            <Stack.Screen name="Select" component={SelectPhoto} />
+          <Stack.Navigator
+            screenOptions={{
+              headerTintColor: "white",
+              headerBackTitleVisible: false,
+              headerBackImage: ({ tintColor }) => (
+                <Ionicons color={tintColor} name="close" size={28} />
+              ),
+              headerStyle: {
+                backgroundColor: "black",
+                shadowOpacity: 0.3,
+              },
+            }}
+          >
+            <Stack.Screen
+              name="SelectPhoto"
+              options={{ title: "Choose a photo" }}
+              component={SelectPhoto}
+            />
           </Stack.Navigator>
         )}
       </Tab.Screen>
--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { styled } from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const Top = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const Bottom = styled.View`
+  flex: 1;
+  background-color: black;
+`;
 
 const SelectPhoto = () => {
   return (
-    <View>
-      <Text>SelectPhoto</Text>
-    </View>
+    <Container>
+      <Top />
+      <Bottom />
+    </Container>
   );
 };
 
