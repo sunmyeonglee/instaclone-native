abbe6b382f98d5130e5c2c0711a58b553e0012ae
vivianlee03 <vivian@u.sogang.ac.kr>
2023-08-12 17:47:04 +0900

    message nav -#19.0

---
+++ screens/Room.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Rooms = () => {
+  return (
+    <View>
+      <Text>Room</Text>
+    </View>
+  );
+};
+
+export default Rooms;
---
+++ screens/Rooms.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Rooms = () => {
+  return (
+    <View>
+      <Text>Rooms</Text>
+    </View>
+  );
+};
+
+export default Rooms;
